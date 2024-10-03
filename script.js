f9358b9824a1b4a401268d0a88858a19c130af60
Rodrigo Goncalves <rodrigo121991@gmail.com>
2024-10-02 21:52:44 -0300

    manipulacao-dom

---
+++ script.js
@@ -0,0 +1,22 @@
+const dadosContatos ={
+"nome": "Rodrigo Gonçalves",
+"email": "fulaninho@email.com",
+"telefone": "46987654321",
+"cpf": "00123456789",
+"endereco": "Rua da fogueira santa",
+"numero": 0,
+"complemento": "Sobrado",
+"cep": 123456789,
+"bairro": "Centro",
+"cidade": "Pato Branco",
+"estado": "Paraná",
+"sexo": "Masculino",
+"estadoCivil": "casado",
+"profissao": "Programador",
+"dataNascimento": "01/01/1980"
+
+}
+
+console.log(dadosPessoa)
+const h1Nome = document.getElementById("nome")
+h1Nome
